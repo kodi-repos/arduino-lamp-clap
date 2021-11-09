2785cc93d652123e3beebd4f63cd5ffe487dbf26
kodi-repos <88886269+kodi-repos@users.noreply.github.com>
2021-11-09 10:24:24 +0600

    full reconstruction

--- main.ino
+++ main.ino
@@ -1,22 +1,32 @@
-int ledpin = // Led port
-int threshold = // Set threshold of
-int volume;
-int knockSensor = A0;
-int sensorReading = 0;
-int ledState = LOW;
+// DONT FORGET TO SET PINS
+
+int count = 0;
+int relay = 0;
+int led = 5;
 
 void setup() {
-	Serial.begin(9600);
-	pinMode(ledPin, OUTPUT);
+	pinMode(3, OUTPUT);
+	pinMode(led, OUTPUT);
 }
 
 void loop() {
-	volume = analogRead(A0);
-
-	if(volume>=threshold){
-		digitalWrite(ledPin, HIGH);
-		ledState = !ledState;
-		digitalWrite(ledPin, ledState);
-		delay(100);
+	digitalWrite(led, LOW);
+	count = analogRead(4);
+	if(count > 200 && count < 600);
+	{
+		delay(250);
+		for(int t=0; t<=500; t++)
+		{
+			delay(1);
+			count=analogRead(4);
+			if(count > 200 && count < 600)
+			{
+				relay = !relay;
+				digitalWrite(led, HIGH);
+				break;
+				delay(200);
+			}
+		}
 	}
+	digitalWrite(3, relay);
 }
